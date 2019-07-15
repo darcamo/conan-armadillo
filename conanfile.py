5155ea250d1ff1c63b926604b84392d38d94c03b
Darlan Cavalcante Moreira <darcamo@gmail.com>
2019-07-15 11:17:34 -0300

    Update armadillo version to 9.600.4

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ import shutil
 
 class ArmadilloConan(ConanFile):
     name = "armadillo"
-    version = "9.500.2"
+    version = "9.600.4"
     license = "Apache License 2.0"
     author = "Darlan Cavalcante Moreira (darcamo@gmail.com)"
     url = "https://github.com/darcamo/conan-armadillo"
