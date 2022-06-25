e7d43f08238d83d11f6be73f66cc6b57af68c3a8
Darlan Cavalcante Moreira <darcamo@gmail.com>
2022-06-25 19:28:38 -0300

    Update armadillo version to 11.2.0

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ from conans import CMake, ConanFile, tools
 
 class ArmadilloConan(ConanFile):
     name = "armadillo"
-    version = "11.1.1"
+    version = "11.2.0"
     license = "Apache License 2.0"
     author = "Darlan Cavalcante Moreira (darcamo@gmail.com)"
     url = "https://github.com/darcamo/conan-armadillo"
